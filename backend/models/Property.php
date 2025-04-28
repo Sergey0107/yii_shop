e565fcbd2816eb07dc38b701b3ef68ed3e9f6a50
Sergey0107 <84847493+Sergey0107@users.noreply.github.com>
2025-04-28 23:34:18 +0300

    add property cruds

---
+++ backend/models/Property.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "property".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ * @property ProductProperty[] $productProperties
+ * @property PropertyValue[] $propertyValues
+ */
+class Property extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'property';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[ProductProperties]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductProperties()
+    {
+        return $this->hasMany(ProductProperty::class, ['property_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[PropertyValues]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPropertyValues()
+    {
+        return $this->hasMany(PropertyValue::class, ['property_id' => 'id']);
+    }
+
+}
