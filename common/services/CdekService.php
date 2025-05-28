910c1a8f88f87f3a1f2fc8a3c66813d90bdf90b3
Sergey0107 <84847493+Sergey0107@users.noreply.github.com>
2025-05-29 00:53:41 +0300

    refactor

--- common/services/CdekService.php
+++ common/services/CdekService.php
@@ -19,9 +19,9 @@ class CdekService
     public function getPickupPoints(int $cityCode): array
     {
 
-        if ($this->isTestMode) {
-            return $this->getTestPoints(55.7558, 37.6173);
-        }
+//        if ($this->isTestMode) {
+//            return $this->getTestPoints(55.7558, 37.6173);
+//        }
 
         try {
             $request = (new DeliveryPoints())
